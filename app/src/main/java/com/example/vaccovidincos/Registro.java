216ba9ab1ada8bc1e32e9c61ee8680443585a109
raul <raulvp9@gmail.com>
2021-11-08 21:46:33 -0400

    cambios en formulario para adicionar calendarios

--- app/src/main/java/com/example/vaccovidincos/Registro.java
+++ app/src/main/java/com/example/vaccovidincos/Registro.java
@@ -1,32 +1,39 @@
 package com.example.vaccovidincos;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class Registro extends AppCompatActivity implements View.OnClickListener {
-    EditText fechaNac;
+public class Registro extends AppCompatActivity {
+    EditText fechaNac,fechaVac;
     private  int dia,mes,anio;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
         fechaNac = (EditText) findViewById(R.id.txtfechanac);
-        fechaNac.setOnClickListener(this);
+        fechaVac = (EditText) findViewById(R.id.txtfechavacuna);
+
+
     }
 
-    @Override
-    public void onClick(View view) {
+    public void fechaNacimiento(View view) {
         final Calendar calendar = Calendar.getInstance();
         dia = calendar.get(Calendar.DAY_OF_MONTH);
         mes = calendar.get(Calendar.MONTH);
         anio = calendar.get(Calendar.YEAR);
+
 
         DatePickerDialog dp = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -36,5 +43,47 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
         },dia,mes,anio);
 
         dp.show();
+
     }
+
+    public void fechaVacuna(View view) {
+
+        final Calendar calendar2 = Calendar.getInstance();
+        dia = calendar2.get(Calendar.DAY_OF_MONTH);
+        mes = calendar2.get(Calendar.MONTH);
+        anio = calendar2.get(Calendar.YEAR);
+
+
+        DatePickerDialog dpv = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                fechaVac.setText(i2 + "/"+(i1+1)+"/"+i);
+            }
+        },dia,mes,anio);
+
+        dpv.show();
+    }
+
+
+
+/*
+
+    @Override
+    public void onClick(View view) {
+        final Calendar calendar = Calendar.getInstance();
+        dia = calendar.get(Calendar.DAY_OF_MONTH);
+        mes = calendar.get(Calendar.MONTH);
+        anio = calendar.get(Calendar.YEAR);
+
+
+        DatePickerDialog dp = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                fechaNac.setText(i2 + "/"+(i1+1)+"/"+i);
+            }
+        },dia,mes,anio);
+
+        dp.show();
+    }*/
+
 }
